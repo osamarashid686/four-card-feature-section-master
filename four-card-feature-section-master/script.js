2154f4c2b5ac28896bee3d09eac41b145dc4fa58
Osama Rashid <89392823+osamarashid686@users.noreply.github.com>
2021-08-25 16:39:59 +0500

    Add files via upload

---
+++ four-card-feature-section-master/script.js
@@ -0,0 +1,11 @@
+function col_changer_1(){
+  return document.body.style.cssText = "background-color: #ffffff";
+}
+
+function col_changer_2(){
+  return document.body.style.cssText = "background-color: #f9e1bc";
+}
+
+function col_changer_3(){
+  return document.body.style.cssText = "background-color: #a7e8e7";
+}
